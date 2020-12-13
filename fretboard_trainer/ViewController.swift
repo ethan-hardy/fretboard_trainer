751781f828c12fb6a306f31b890d8336f94d80d5
ethan hardy <ehardy45@gmail.com>
2020-12-13 13:10:31 -0500

    Initial Commit

---
+++ fretboard_trainer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fretboard_trainer
+//
+//  Created by Ethan Hardy on 2020-12-13.
+//  Copyright © 2020 Ethan Hardy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
